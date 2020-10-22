cdc9ffd4817ef68b7577d56c9186c5019a364e61
balireddymadhu <madhusudhanb238@gmail.com>
2020-10-22 10:43:07 +0530

    changes

---
+++ reddyproject/newapp/admin.py
@@ -0,0 +1,5 @@
+from django.contrib import admin
+from .models import Madhu
+
+admin.site.register(Madhu)
+# Register your models here.
---
+++ reddyproject/newapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+class Madhu(models.Model):
+    name = models.CharField(max_length=50)
+    email = models.EmailField(blank=True, null=True)
+    age = models.PositiveIntegerField()
+    Remarks = models.TextField()
+    about = models.TextField()
+
+    def __str__(self):
+        return self.name
